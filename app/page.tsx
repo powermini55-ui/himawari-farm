85a80d772665cc567644902dda90f7e4d2e087e2
powermini55-ui <powermini55@gmail.com>
2026-02-27 17:01:30 +0900

    Add Home component with farm information

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>ひまわりファーム</h1>
+      <p>栽培期間中農薬不使用</p>
+
+      <h2>お問い合わせ</h2>
+      <p>Instagram: @himawari_farm17</p>
+    </main>
+  );
+}
